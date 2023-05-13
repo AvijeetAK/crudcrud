3dc25e1b34c0ccbf3d23ce53cfec38c6bebd7273
AvijeetAk <avijeetkumar1994@gmail.com>
2023-05-13 20:06:31 +0530

    main

--- main.js
+++ main.js
@@ -2,15 +2,35 @@ var newform = document.querySelector('#newform');
 var user = document.querySelector('#user');
 var roll = document.querySelector('#roll');
 var submit = document.querySelector('#submit');
+var newuser = document.querySelector('#newuser');
 
 newform.addEventListener('submit', onSubmit);
+
 
 
 function onSubmit(e)
 {
     e.preventDefault();
     console.log(user.value);
-    axios.post('https://crudcrud.com/api/6561833caca740b1a4f2be2dc05326d4/appointment', {title : user.value})
+    axios.post('https://crudcrud.com/api/11e280c6aeec455ebf31241b728a52ec/appointment', {title : user.value})
     .then( res => console.log(res))
     .catch(err => console.log(err))
 }
+
+window.addEventListener("DOMContentLoaded", () => {
+
+    axios.get('https://crudcrud.com/api/11e280c6aeec455ebf31241b728a52ec')
+    .then((res) => {
+        for(var i=0; i<res.data.length; i++)
+        {
+        var txt = document.createTextNode(res.data[i]);
+        var li = document.createElement(li);
+        li.appendChild(txt);
+        newuser.appendChild(li);
+        }
+    })
+    .catch(err => console.log(err))
+
+})
+
+
